04243241e576f08318a72da17c39b88260244967
Omar Menchaca <megafomar@gmail.com>
2020-05-22 22:26:44 -0500

    Updates

---
+++ precache-manifest.cfb1f5faa2eb0dec96c3af1bfc5b0f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f73c2aa1a4fe2ac260d2ac22a334fdd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "81c3fd6f46830c8fbdbf",
+    "url": "/monster-rolodex/static/css/main.692ff694.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monster-rolodex/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c3fd6f46830c8fbdbf",
+    "url": "/monster-rolodex/static/js/main.47c6b455.chunk.js"
+  },
+  {
+    "revision": "8299370f6f3fcee7b4fa",
+    "url": "/monster-rolodex/static/js/runtime-main.6608e69f.js"
+  }
+]);
